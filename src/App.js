da80d69370fe0c7e382ce729ea126caba4dc96fb
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-02-06 10:17:46 -0500

    imported components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import About from "./about.js";
+import Navbar from "./navbar.js";
+import Footer from "./footer.js";
+import Sidebar from "./sidebar.js";
+// const url = "https://course-api.com/react-tours-project";
 const App = () => {
-  return <div>raman gill</div>;
+  return (
+    <>
+      <div>
+        <h1>rtodayaman</h1>
+      </div>
+      <About />
+      <Navbar />
+      <Footer />
+      <Sidebar />
+    </>
+  );
 };
 
 export default App;
---
+++ src/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>raman</h3>
+    </div>
+  );
+};
+
+export default About;
